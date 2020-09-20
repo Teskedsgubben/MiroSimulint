3cf91454e44ff709cbf8f29110ed643d3ae7d629
Felix <f3liix@hotmail.com>
2020-09-20 22:06:31 +0200

    infoboard added

--- src/MIT_Props.py
+++ src/MIT_Props.py
@@ -17,6 +17,7 @@ def AddProps(system):
 
     props.painting(system, [13.8,2.0,-2], 'DemoBengan.png', wall_angle)
     props.painting(system, [13.94,1.8,-3.35], 'bungeebengan_notes.png', wall_angle, [0.2,0.27])
+    props.painting(system, [6.46,5.0,10], 'infoboard.jpg', -90, [0.85/2, 1.46/2])
     props.painting(system, [-3.5,2.1,5], 'walkplanck.png', 0, [0.8,0.6])
     props.pokeball(system, [1.75,0.85,-7.15], 0)
     props.sodacan(system, [1.25,0.85,-7.05], 'schrodbull.png', 180)
