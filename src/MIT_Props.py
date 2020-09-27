3fec46da0b134497981aea08693ed8841d1cb3a4
Felix <f3liix@hotmail.com>
2020-09-27 22:43:06 +0200

    added some props

--- src/MIT_Props.py
+++ src/MIT_Props.py
@@ -7,7 +7,7 @@ def AddProps(system):
 
     props.MIT_door(system, [-5, 3.3, 8.22])
 
-    props.MIT_door(system, [13.04, 3.3, 5.5], wall_angle)
+    props.MIT_door(system, [13.49,3.3, 1.2], wall_angle)
     props.MIT_door(system, [14.48, 3.3, -8.5], wall_angle)
     
     props.MIT_door(system, [13.04, 6.6, 5.5], wall_angle)
@@ -24,7 +24,16 @@ def AddProps(system):
 
     props.painting(system, [13.8,2.0,-2], 'DemoBengan.png', wall_angle)
     props.painting(system, [13.94,1.8,-3.35], 'bungeebengan_notes.png', wall_angle, [0.2,0.27])
+
     props.painting(system, [6.46,5.0,10], 'infoboard.jpg', -90, [0.85/2, 1.46/2])
+    props.painting(system, [13.34,2.0,2.2], 'floorinfo2.jpg', wall_angle, [0.6,0.66])
+
+    props.painting(system, [12.84,5.1,7.1], 'corkboard.jpg', wall_angle, [1.0,0.5])
+    props.painting(system, [13.047,5.1,5.1], 'corkboard.jpg', wall_angle, [1.0,0.5])
+    
+    props.painting(system, [13.77,5.1,-1.9], 'corkboard.jpg', wall_angle, [1.0,0.5])
+    props.painting(system, [13.977,5.1,-3.9], 'corkboard.jpg', wall_angle, [1.0,0.5])
+
     props.painting(system, [-3.5,2.1,5], 'walkplanck.png', 0, [0.8,0.6])
     props.pokeball(system, [1.75,0.85,-7.15], 0)
     props.sodacan(system, [1.25,0.85,-7.05], 'schrodbull.png', 180)
