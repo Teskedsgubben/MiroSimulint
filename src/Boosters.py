d74d12c04a394dd79a601cb47874f3fdf08b16e1
Felix <f3liix@hotmail.com>
2020-09-28 23:26:07 +0200

    Enabled controlling amount of pulses used

--- src/Boosters.py
+++ src/Boosters.py
@@ -3,13 +3,18 @@ import numpy as np
 
 from MiroClasses import MiroComponent as mc
 
-def MCB01(trigger_function, rot = [0,0,0], pos = [0,0,0], Fixed = False):
+def MCB01(trigger_function, pulses = 1, rot = [0,0,0], pos = [0,0,0], Fixed = False):
     '''A booster that emits a pulse when the trigger function returns True.\n
     The trigger function must follow the form:\n
     def trigger_function(position, velocity, acceleration)'''
+    if pulses > 5:
+        pulses = 5
+    if pulses < 0:
+        pulses = 0
+        
     size_h = 0.05
     size_r = 0.0075
-    density_brick = 0.05/(np.pi*size_r**2*size_h)   # kg/m^3
+    density_brick = 0.02/(np.pi*size_r**2*size_h)   # kg/m^3
 
     body_brick = chrono.ChBodyEasyCylinder(size_r, size_h, density_brick)
     body_brick.SetBodyFixed(False)
@@ -29,8 +34,8 @@ def MCB01(trigger_function, rot = [0,0,0], pos = [0,0,0], Fixed = False):
     # Generate MiroComponent with above ChBody
     BOOSTER = mc.MiroBooster(body_brick)
     BOOSTER.SetTrigger(trigger_function)
-    BOOSTER.SetConsumption(1000)
-    BOOSTER.SetForce(0.5, 0.4, [0,-size_h/2,0])
+    BOOSTER.SetFuel(400*pulses)
+    BOOSTER.SetForce(0.5)
 
     BOOSTER.AddLinkPoint('A', [ 1, 0, 0], chrono.ChVectorD( size_r, size_h/2-0.01, 0))
     BOOSTER.AddLinkPoint('B', [ 1, 0, 0], chrono.ChVectorD( size_r,-size_h/2+0.01, 0))
