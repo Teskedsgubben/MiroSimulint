0b2ecaa46b500e23581249dbaa37ef83a1a39223
Niklas <niklasedlund1@gmail.com>
2020-10-04 13:59:27 +0200

    all windows complete

--- src/MIT_CSplan4.py
+++ src/MIT_CSplan4.py
@@ -75,29 +75,34 @@ def build(MiroSystem, SPEEDMODE = False):
         
 
         #horizontal windowpanes
-        h_windowpane_size = np.array([0.05, 4.8, 0.01])     #horizontal window pane size
+        h_windowpane_size = np.array([0.05, 4.25, 0.01])     #horizontal window pane size
 
         #2nd window horizontal
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [1.51, 2*H+0.015, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [1.51, 2*H+0.8, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [1.51, pos_up[1] - 0.5, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [1.51, 2*H+3, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.6, h_windowpane_size[2], [1.51, 2*H+0.015, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.6, h_windowpane_size[2], [1.51, 2*H+0.8, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.6, h_windowpane_size[2], [1.51, pos_up[1] - 0.5, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.6, h_windowpane_size[2], [1.51, 2*H+3, 4.98], rotZ=90, texture='wood_ikea_style.png')
         #1st window horizontal
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [-3, 2*H+0.015, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [-3, 2*H+0.8, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [-3, pos_up[1] - 0.5, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1], h_windowpane_size[2], [-3, 2*H+3, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.36, h_windowpane_size[2], [-3, 2*H+0.015, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.36, h_windowpane_size[2], [-3, 2*H+0.8, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.36, h_windowpane_size[2], [-3, pos_up[1] - 0.5, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]+0.36, h_windowpane_size[2], [-3, 2*H+3, 4.98], rotZ=90, texture='wood_ikea_style.png')
         #3rd window horizontal
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2, h_windowpane_size[2], [5.25, 2*H+0.015, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2, h_windowpane_size[2], [5.25, 2*H+0.8, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2, h_windowpane_size[2], [5.25, pos_up[1] - 0.5, 4.98], rotZ=90, texture='wood_ikea_style.png')
-        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2, h_windowpane_size[2], [5.25, 2*H+3, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2+0.3, h_windowpane_size[2], [5.25, 2*H+0.015, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2+0.3, h_windowpane_size[2], [5.25, 2*H+0.8, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2+0.3, h_windowpane_size[2], [5.25, pos_up[1] - 0.5, 4.98], rotZ=90, texture='wood_ikea_style.png')
+        MiroAPI.add_boxShape(MiroSystem, h_windowpane_size[0], h_windowpane_size[1]/2+0.3, h_windowpane_size[2], [5.25, 2*H+3, 4.98], rotZ=90, texture='wood_ikea_style.png')
 
-        test_pos = np.array([posS[0]+0.1, -0.16, posS[2]])        
-        make_window(v_windowpane_size[0], v_windowpane_size[1], v_windowpane_size[2]+0.1, test_pos, 'south', 0.685, MiroSystem)
+        test_posS = np.array([posS[0]+0.1, -0.16, posS[2]])        
+        test_posE = np.array([posE[0], -0.16, posE[2]])
 
+        make_window(v_windowpane_size[0], v_windowpane_size[1], v_windowpane_size[2], h_windowpane_size[0],
+        h_windowpane_size[1], h_windowpane_size[2], test_posS, 'south', 0.685, MiroSystem)
+        
+        make_window(v_windowpane_size[0], v_windowpane_size[1], v_windowpane_size[2], h_windowpane_size[0],
+        h_windowpane_size[1], h_windowpane_size[2], test_posE, 'east', 0.685, MiroSystem)
 
-def make_window(size_vx, size_vy, size_vz, mid_pos, wall, spacing, MiroSystem): #size_hx, size_hy, size_hz
+def make_window(size_vx, size_vy, size_vz, size_hx, size_hy, size_hz, mid_pos, wall, spacing, MiroSystem): #size_hx, size_hy, size_hz
     #wall sets orientation of window, set wall = 'south' or 'east'
     # pos sets position 
     #and spacing sets the spacing between windowpanes
@@ -106,6 +111,7 @@ def make_window(size_vx, size_vy, size_vz, mid_pos, wall, spacing, MiroSystem): 
     dists = np.array([97,207,358,436,510,654,802,876,951,1102,1213])
     dists = dists - (dists[-1] + dists[0])/2    
     dists = dists / dists[-1] * (W-3*0.16)/2
+    test = 0.085
 
     if wall == 'south':
         for i in range(1, 6, 2):
@@ -115,9 +121,34 @@ def make_window(size_vx, size_vy, size_vz, mid_pos, wall, spacing, MiroSystem): 
                 for k in range(0, len(dists)):
                     tmp = np.array([0, 0, dists[k]])
                     result_pos = mid_pos + lvl_width + lvl_height + tmp
-                    MiroAPI.add_boxShape(MiroSystem, size_vx, size_vy, size_vz, result_pos)
+                    MiroAPI.add_boxShape(MiroSystem, size_vx, size_vy, size_vz, result_pos, rotY=90,texture='wood_ikea_style.png')
+                for n in [-1, 1]:
+                    hzt_pos = np.array([0.02, 3/2, 0])
+                    r_hzt_pos = mid_pos + n*hzt_pos + lvl_height + lvl_width
+                    MiroAPI.add_boxShape(MiroSystem, size_hx, size_hy-test, size_hz, r_hzt_pos, rotX=90,rotZ=90,texture='wood_ikea_style.png')
+                    if n == -1:
+                        MiroAPI.add_boxShape(MiroSystem, size_hx, size_hy-test, size_hz, r_hzt_pos+np.array([0,0.8*n*(-1),0]) ,rotX=90,rotZ=90,texture='wood_ikea_style.png')
+                    else:
+                        MiroAPI.add_boxShape(MiroSystem, size_hx, size_hy-test, size_hz, r_hzt_pos+np.array([0,0.7*n*(-1),0]) ,rotX=90,rotZ=90,texture='wood_ikea_style.png')
+    if wall == 'east':
+        for i in range(1, 6, 2):
+            lvl_height = np.array([0, i/2*H, 0])
+            for j in [-1, 0, 1]:
+                lvl_width = np.array([j*W, 0, 0])
+                for k in range(0, len(dists)):
+                    tmp = np.array([dists[k], 0, 0])
+                    result_pos = mid_pos + lvl_width + lvl_height + tmp
+                    MiroAPI.add_boxShape(MiroSystem, size_vz,  size_vy, size_vx, result_pos, rotY=90,texture='wood_ikea_style.png')
+                for n in [-1, 1]:
+                    hzt_pos = np.array([0, 3/2, 0.02])
+                    r_hzt_pos = mid_pos + n*hzt_pos + lvl_height + lvl_width
+                    MiroAPI.add_boxShape(MiroSystem, size_hx, size_hy-test, size_hz, r_hzt_pos, rotZ=90,texture='wood_ikea_style.png')
+                    if n == -1:
+                        MiroAPI.add_boxShape(MiroSystem, size_hx, size_hy-test, size_hz, r_hzt_pos+np.array([0,0.8*n*(-1),0]), rotZ=90,texture='wood_ikea_style.png')
+                    else:
+                        MiroAPI.add_boxShape(MiroSystem, size_hx, size_hy-test, size_hz, r_hzt_pos+np.array([0,0.7*n*(-1),0]), rotZ=90,texture='wood_ikea_style.png')
 
-
+                     
 
     
     
