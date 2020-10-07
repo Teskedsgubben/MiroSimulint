5195d2bd71f7eecc7416eed0ba7f5f1ef54947b8
Felix <f3liix@hotmail.com>
2020-10-07 18:14:15 +0200

    now goes through MiroAPI

--- MiroClasses/MiroNotifier.py
+++ MiroClasses/MiroNotifier.py
@@ -1,53 +1,25 @@
-import pychrono.core as chrono
+from MiroClasses.MiroAPI_selector import SelectedAPI as MiroAPI
 import numpy as np
 
 class MiroNotifier():
     def __init__(self, pos, angle = 0, tilt = 0):
-        x = 4
-        y = 1.5
-        r = 0.35
+        self.width = 4
+        self.height = 1.5
+        self.radius = 0.35
+        self.turn_angle = np.deg2rad(angle)
+        self.tilt_angle = np.deg2rad(tilt)
+        self.position = np.array(pos)
 
-        self.tilt_Q = chrono.Q_from_AngAxis(np.deg2rad(tilt), chrono.ChVectorD(1,0,0))
-        self.angle_Q = chrono.Q_from_AngAxis(np.deg2rad(angle), chrono.ChVectorD(0,1,0))
-
-        bulbvec = self.angle_Q.Rotate(self.tilt_Q.Rotate(chrono.ChVectorD(x/2 - 1.6*r, y/2 - 1.6*r, 0)))
-
-        self.board = chrono.ChBody()
-        self.board.SetBodyFixed(True)
-        self.board.SetCollide(False)
-        self.board.SetPos(chrono.ChVectorD(pos[0], pos[1], pos[2]))
-        self.board.SetRot(self.angle_Q * self.tilt_Q)
-
-        # Visualization shape
-        board_shape = chrono.ChBoxShape()
-        board_shape.GetBoxGeometry().Size = chrono.ChVectorD(x/2, y/2, 0.05)
-        self.board.GetAssets().push_back(board_shape)
-
-        self.signalbulb = chrono.ChBodyEasySphere(r, 500)
-        self.signalbulb.SetBodyFixed(True)
-        self.signalbulb.SetPos(chrono.ChVectorD(pos[0], pos[1], pos[2]) + bulbvec)
-        self.signalbulb.SetRot(self.angle_Q * self.tilt_Q * chrono.Q_from_AngAxis(np.deg2rad(90), chrono.ChVectorD(1,0,0)))
-
-        # Collision shape
-        self.signalbulb.SetCollide(True)
-        self.signalbulb.GetCollisionModel().ClearModel()
-        self.signalbulb.GetCollisionModel().AddSphere(r)
-        self.signalbulb.GetCollisionModel().BuildModel()
-
+        bulb_pos_hrz = np.array([np.cos(self.turn_angle ), 0, -np.sin(self.turn_angle)]) * (self.width/2 - 1.6*self.radius)
+        bulb_pos_vrt = np.array([np.cos(self.turn_angle +np.pi/2)*np.sin(self.tilt_angle), np.cos(self.tilt_angle), -np.sin(self.turn_angle +np.pi/2)*np.sin(self.tilt_angle)]) * (self.height/2 - 1.6*self.radius)
+        self.bulb_pos = bulb_pos_hrz + bulb_pos_vrt
 
     def AddToSystem(self, system):
-        board_texture = chrono.ChTexture(chrono.GetChronoDataFile('textures/notifboard.png'))
-        board_texture.SetTextureScale(4, 3)
-        self.board.GetAssets().push_back(board_texture)
-        system.Add_Object(self.board)
-        
-        bulb_texture = chrono.ChTexture(chrono.GetChronoDataFile('textures/notifbulb.png'))
-        bulb_texture.SetTextureScale(1, 1)
-        self.signalbulb.GetAssets().push_back(bulb_texture)
-        system.Add_Object(self.signalbulb)
+        MiroAPI.add_boxShape(system, self.width, self.height, 0.05, self.position, 'textures/notifboard.png', rotX=self.tilt_angle, rotY=self.turn_angle, rotDegrees=False)
+        self.bulb = MiroAPI.add_sphereShape(system, self.radius, self.position+self.bulb_pos, 'textures/notifbulb.png', rotX=self.tilt_angle+np.pi/2, rotY=self.turn_angle, rotDegrees=False)
 
 
     def Set_Idle(self):
-        self.signalbulb.SetRot(self.angle_Q * self.tilt_Q * chrono.Q_from_AngAxis(np.deg2rad(90), chrono.ChVectorD(1,0,0)))
+        MiroAPI.rotateBody(self.bulb, rotAngle=180, rotAxis=self.bulb_pos)
     def Set_Ready(self):
-        self.signalbulb.SetRot(self.angle_Q * self.tilt_Q * chrono.Q_from_AngAxis(np.deg2rad(-90), chrono.ChVectorD(1,0,0)))
+        MiroAPI.rotateBody(self.bulb, rotAngle=-180, rotAxis=self.bulb_pos)
