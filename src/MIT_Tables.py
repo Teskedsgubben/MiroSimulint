8063236f28c6ef2984eab45006aa7e9ed6f34092
Felix <f3liix@hotmail.com>
2020-10-10 04:52:11 +0200

    tables and charis can now move

--- src/MIT_Tables.py
+++ src/MIT_Tables.py
@@ -2,6 +2,8 @@ from MiroClasses.MiroAPI_selector import SelectedAPI as MiroAPI
 import numpy as np
 import random
 
+FIXED = False
+
 def build(ChSystem, SPEEDMODE = False):
     if not SPEEDMODE:
         second_floor(ChSystem)
@@ -88,44 +90,47 @@ def fourth_floor(MiroSystem):
             pos_chair = table_pos + n
             MIT_chair(MiroSystem, pos_chair,chair_i)  
 
-def MIT_table(MiroSystem, table_pos, size_table_x, size_table_y, size_table_z,size_leg_h, size_leg_r):    
-    size_table = np.array([size_table_x, size_table_y, size_table_z, size_leg_h])
-    tabletop(MiroSystem, table_pos, size_table)
-    length = np.sqrt((size_table_x/2)**2 + (size_table_z/2)**2) - 2*size_leg_r
-    for i in range(4):
-        theta = i*0.5*np.pi + np.pi/4
-        n = np.array([length*np.cos(theta), size_leg_h/2, length*np.sin(theta)])
-        leg_pos = table_pos + n
-        table_leg(MiroSystem,leg_pos, size_leg_r, size_leg_h)
-
-
-
 def tabletop(MiroSystem, table_pos, size_table):
     if(size_table[0] < 0.6):
         tex = 'textures/MITstol.jpg'
     else:
         tex = 'textures/MITbord.jpg'
-
-    MiroAPI.add_boxShape(MiroSystem, size_table[0], size_table[1], size_table[2], table_pos + np.array([0,size_table[3],0]), tex)
-    
+    pos = table_pos + np.array([0,size_table[1]/2 + size_table[3],0])
+    return MiroAPI.add_boxShape(MiroSystem, size_table[0], size_table[1], size_table[2], pos, tex, Fixed=FIXED)
 
 def table_leg(MiroSystem, leg_pos, size_leg_r, size_leg_h):
     # creating the cylinder leg
-    MiroAPI.add_cylinderShape(MiroSystem, size_leg_r, size_leg_h, 1200, leg_pos, texture='brushsteel.png')
+    return MiroAPI.add_cylinderShape(MiroSystem, size_leg_r, size_leg_h, 1200, leg_pos, texture='brushsteel.png', Fixed=FIXED)
+
+def chair_back(MiroSystem, pos_back, size_back_x,size_back_y, size_back_z):
+    return MiroAPI.add_boxShape(MiroSystem, size_back_x, size_back_y, size_back_z, pos_back, 'MITstol.jpg', Fixed=FIXED)
+
+def MIT_table(MiroSystem, table_pos, size_table_x, size_table_y, size_table_z,size_leg_h, size_leg_r):    
+    size_table = np.array([size_table_x, size_table_y, size_table_z, size_leg_h])
+    top = tabletop(MiroSystem, table_pos, size_table)
+    length = np.sqrt((size_table_x/2)**2 + (size_table_z/2)**2) - 2*size_leg_r
+    for i in range(4):
+        theta = i*0.5*np.pi + np.pi/4
+        n = np.array([length*np.cos(theta), size_leg_h/2, length*np.sin(theta)])
+        leg_pos = table_pos + n
+        leg = table_leg(MiroSystem,leg_pos, size_leg_r, size_leg_h)
+        if not FIXED:
+            MiroSystem.Add(MiroAPI.LinkBodies_Hinge(top, leg, leg_pos+np.array([0,size_leg_h,0]), [0,1,0]))
+    return top
 
 def MIT_chair(MiroSystem, pos_chair,rotation):
     #dimensions of the chair
     size_chair_x = 0.5
     size_chair_y = 0.1
     size_chair_z = 0.5
-    size_chair_leg_h = 0.5
+    size_chair_leg_h = 0.42
     size_leg_r = 0.02 
     size_back_cylinder_r = 0.015 # the back of the chair
-    size_back_cylinder_h = 0.5
+    size_back_cylinder_h = 0.38
     size_back_x = size_chair_x/7
     size_back_y = 0.2
     size_back_z = size_chair_z + 0.05 
-    MIT_table(MiroSystem, pos_chair, size_chair_x, size_chair_y, size_chair_z, size_chair_leg_h, size_leg_r) # making the bottom of the chair
+    seat = MIT_table(MiroSystem, pos_chair, size_chair_x, size_chair_y, size_chair_z, size_chair_leg_h, size_leg_r) # making the bottom of the chair
     
     if rotation == 0:
         pos_back  = pos_chair + np.array([-size_chair_x/2.3, size_back_cylinder_h + size_chair_leg_h,0])
@@ -142,16 +147,19 @@ def MIT_chair(MiroSystem, pos_chair,rotation):
         size_back_x = size_back_z
         size_back_z = temp 
    
-    chair_back(MiroSystem, pos_back, size_back_x,size_back_y, size_back_z)
+    pos_back = pos_back + np.array([0, size_chair_y+size_back_y/2, 0])
+    neck = chair_back(MiroSystem, pos_back, size_back_x,size_back_y, size_back_z)
     length_0 = np.sqrt((size_chair_x/2)**2 + (size_chair_z/2)**2) - 2*size_back_cylinder_r
     theta_0 = rotation*0.5*np.pi - np.pi/4
     
     for i in range(6):
         theta = theta_0 + i*np.pi/10
         length = length_0 - (i*(5-i)*(1-1/np.sqrt(2))*length_0)/6.25 
-        n = np.array([- length*np.cos(theta), size_back_cylinder_h/2 + size_chair_leg_h, length*np.sin(theta)])
+        n = np.array([- length*np.cos(theta), size_back_cylinder_h/2 + size_chair_leg_h + size_chair_y, length*np.sin(theta)])
         pos_back = pos_chair + n
-        table_leg(MiroSystem,pos_back, size_back_cylinder_r, size_back_cylinder_h)
+        leg = table_leg(MiroSystem,pos_back, size_back_cylinder_r, size_back_cylinder_h)
+        if not FIXED:
+            MiroSystem.Add(MiroAPI.LinkBodies_Hinge(seat, leg, pos_back-np.array([0,size_back_cylinder_h,0]), [0,1,0]))
+            MiroSystem.Add(MiroAPI.LinkBodies_Hinge(neck, leg, pos_back+np.array([0,size_back_cylinder_h,0]), [0,1,0]))
 
-def chair_back(MiroSystem, pos_back, size_back_x,size_back_y, size_back_z):
-    MiroAPI.add_boxShape(MiroSystem, size_back_x, size_back_y, size_back_z, pos_back, 'MITstol.jpg')
+
