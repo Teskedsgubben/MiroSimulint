1ea28be98bb57d9d4465d1a14bddda39bfcca279
Felix <f3liix@hotmail.com>
2020-10-16 00:21:26 +0200

    added ramp in one of the roms

--- src/MIT_Offices_East.py
+++ src/MIT_Offices_East.py
@@ -2,4 +2,8 @@ from MiroClasses.MiroAPI_selector import SelectedAPI as MiroAPI
 import numpy as np
 
 def build(MiroSystem, SPEEDMODE = False):
+    Plan4_Rum3(MiroSystem)
     return
+
+def Plan4_Rum3(MiroSystem):
+    MiroAPI.add_boxShape(MiroSystem, 4, 0.85, 3, [-4.5, 6.64, -10.3], rotX=-16, friction=2000)
