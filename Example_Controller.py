bad91de6b689979941ef5ac49c2b295e035dae75
Felix <f3liix@hotmail.com>
2021-05-01 19:15:37 +0200

    remove agx imports and reworked the controls structures to a more intuitive use example

--- Example_Controller.py
+++ Example_Controller.py
@@ -1,80 +1,86 @@
-# Example controller
-
 # An example controller for a robot. It has a very basic setup, 
 # and is meant to function as a prototype for how a controller 
 # can be implemented. 
-
-
-import agx
-import agxCollide
-import agxOSG
-import agxSDK
-import agxPython
-import agxIO
-import agxModel
-import agxRender
-
-
 from MiroClasses.MiroAPI_selector import SelectedAPI as MiroAPI
-from MiroClasses import MiroAPI_agx as agxAPI 
 
 
-#Setup for keys that will be used
-controls = {
-    'KEY_UP': agxSDK.GuiEventListener.KEY_Up,
-    'KEY_DOWN': agxSDK.GuiEventListener.KEY_Down,
-    'KEY_LEFT': agxSDK.GuiEventListener.KEY_Left,
-    'KEY_RIGHT': agxSDK.GuiEventListener.KEY_Right,
-    'SPACE': agxSDK.GuiEventListener.KEY_Space,
-    'DELETE': agxSDK.GuiEventListener.KEY_Delete,
-    'HOME': agxSDK.GuiEventListener.KEY_Home,
-    'PAGE_UP': agxSDK.GuiEventListener.KEY_Page_Down,
-    'PAGW_DOWN': agxSDK.GuiEventListener.KEY_Page_Up,
-    'INSERT': agxSDK.GuiEventListener.KEY_Insert,
-    'END': agxSDK.GuiEventListener.KEY_End
+# Setup for keys that will be used. This is stored in the module in the main function, 
+# so you can easily create different control layouts to try out.
+controls_arrows = {
+    'Forward': MiroAPI.Keyboard('KEY_UP'),
+    'Backward': MiroAPI.Keyboard('KEY_DOWN'),
+    'Left': MiroAPI.Keyboard('KEY_LEFT'),
+    'Right': MiroAPI.Keyboard('KEY_RIGHT'),
+    'Brake': MiroAPI.Keyboard('SPACE'),
+    'Print_Example': MiroAPI.Keyboard('HOME')
+}
+
+# You can use letter keys as well, but keep in mind they may collide with other commands
+# In this WASD example, the 'w' key overlaps with built in functions in agxViewer
+controls_wasd = {
+    # Tip: Using upper case letters like 'W' will bind Shift + 'w'
+    'Forward': MiroAPI.Keyboard('w'),
+    'Backward': MiroAPI.Keyboard('s'),
+    'Left': MiroAPI.Keyboard('a'),
+    'Right': MiroAPI.Keyboard('d'),
+    'Brake': MiroAPI.Keyboard('SPACE'),
+    'Print_Example': MiroAPI.Keyboard('HOME')
 }
 
 
-#Controller requires these input arguments to function, removing arguments or changing order will result in errors
+###################################################
+############## GUI BASED CONTROLLER ###############
+###################################################
+#
+# The controller function will recieve these input arguments when called
+# Removing arguments or changing order will result in errors
 def MyController(module, keydown, key, alt):
-
-    #Input arguments
-    #module: Module to control
-    #keydown: Boolean, true when key is pressed, false when released
-    #key: Key pressed, latest key pressed if multiple keys are pressed simultaniously
-    #alt: Alternative key pressed, 'alt', 'shift' etc.  
-
-    #Arguments in module.SetMotorSpeed('LINK_NAME', SPEED)
-    #Link needs to have motor enabled
+    # Input arguments
+    # module: Module to control
+    # keydown: Boolean, true when key is pressed, false when released
+    # key: Key pressed, latest key pressed if multiple keys are pressed simultaniously
+    # alt: Alternative key pressed, 'alt', 'shift' etc.  
 
     if not keydown:
-        
-        module.SetMotorSpeed('FL_tire', 0)
-        module.SetMotorSpeed('FR_tire', 0)
+        # Do not apply force if no key is pressed by setting speed=None
+        module.SetMotorSpeed('FL_tire', None)
+        module.SetMotorSpeed('FR_tire', None)
     else:
-        if key == module.controls['KEY_UP']:
+        # The controls you choose when adding the controller to the module will be accessible 
+        # through module.controls['CONTROL_NAME']
+        if key == module.controls['Print_Example']:
+            print("You pressed the 'Print_Example' button!")
+
+        elif key == module.controls['Forward']:
+            # Set arguments in module.SetMotorSpeed('LINK_NAME', SPEED)
+            # Link needs to have motor enabled. The speed values have 
+            # opposing signs due to left and right orientetions.
             module.SetMotorSpeed('FL_tire', -50)
             module.SetMotorSpeed('FR_tire', 50)
                 
-        elif key == module.controls['KEY_DOWN']:
+        elif key == module.controls['Backward']:
             module.SetMotorSpeed('FL_tire', 15)
             module.SetMotorSpeed('FR_tire', -15)
 
-        elif key == module.controls['KEY_LEFT']:
+        elif key == module.controls['Left']:
             module.SetMotorSpeed('FL_tire', -3)
             module.SetMotorSpeed('FR_tire', 25)
     
-        elif key == module.controls['KEY_RIGHT']:
+        elif key == module.controls['Right']:
             module.SetMotorSpeed('FL_tire', -25)
             module.SetMotorSpeed('FR_tire', 3)
 
-        elif key == module.controls['SPACE']:
+        elif key == module.controls['Brake']:
+            # Brake by forcing 0 speed
             module.SetMotorSpeed('FL_tire', 0)
             module.SetMotorSpeed('FR_tire', 0)
 
-
-
-
+###################################################
+############# SELF DRIVING CONTROLLER #############
+###################################################
+#
+# The self driving function only recieves the module itself as an input
+# You can access motors, sensors etc from their names in the module
 def SensorController(module):    
     # Get sensor that we called 'Lidar1' when building the bot
     Front_Sensor=module.GetSensor('Lidar1')
@@ -82,8 +88,8 @@ def SensorController(module):
     # Get measurements from sensor as list
     d = Front_Sensor.GetDistances()
 
-    # Prints distances for debugging:
-    print("print distances: d =", d)
+    # You can print distances for debugging:
+    # print("print distances: d =", d)
     
     # Check if obstacle straight ahead
     if d[1] > 0.35: 
@@ -119,8 +125,8 @@ def SensorController(module):
 
     # If no obstacle move forward
     if left_clear and right_clear:
-        module.SetMotorSpeed('FL_tire', -50)
-        module.SetMotorSpeed('FR_tire', 50)
+        module.SetMotorSpeed('FL_tire', -25)
+        module.SetMotorSpeed('FR_tire', 25)
         return
 
     # Narrow path 
