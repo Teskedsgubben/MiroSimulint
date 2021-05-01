a3ffaf33527abda10a89ca29d542c9cfa2a698b7
Felix <f3liix@hotmail.com>
2021-05-01 19:17:39 +0200

    reworked to enable pylint to find the API of the currently used API, since differences currently exist

--- MiroClasses/MiroAPI_selector.py
+++ MiroClasses/MiroAPI_selector.py
@@ -12,22 +12,22 @@
 # to install all dependencies into the same environment.
 
 # Set to 'PyChrono' or 'AGX'
+API = 'AGX'
 
 import os
-if(os.path.isfile('MiroClasses/MiroAPI_local.py')):
-    from MiroClasses import MiroAPI_local
-    SelectedAPI = MiroAPI_local.SelectedAPI
-else:
-    API = 'AGX'
-
-    if API == 'AGX':
-        import MiroClasses.MiroAPI_agx as agxAPI
-        SelectedAPI = agxAPI
-    elif API == 'PyChrono':
-        import MiroClasses.MiroAPI_chrono as chronoAPI
-        SelectedAPI = chronoAPI
+if(not os.path.isfile('MiroClasses/MiroAPI_local.py')):
+    if API == 'PyChrono':
+        import MiroClasses.MiroAPI_chrono as SelectedAPI
+    elif API == 'AGX':
+        import MiroClasses.MiroAPI_agx as SelectedAPI
     else:
         import sys
         sys.exit('API configuration invalid, open MiroClasses -> MiroAPI_selector to fix.')
+else:
+    from MiroClasses import MiroAPI_local
+    SelectedAPI = MiroAPI_local.SelectedAPI
 
-
+# To create a local API configuration file, simply create a file in the same directory 
+# named 'MiroAPI_local.py' that contains only one of these lines:
+# import MiroClasses.MiroAPI_chrono as SelectedAPI
+# import MiroClasses.MiroAPI_agx as SelectedAPI
